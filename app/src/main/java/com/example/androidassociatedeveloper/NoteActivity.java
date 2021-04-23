e272107b78a87d73fad7ad66cb19915650cef41a
APIYOJENNIFER <apiyojennifer4@gmail.com>
2021-04-23 23:11:07 +0300

    using parcelables to transfer data between activities

--- app/src/main/java/com/example/androidassociatedeveloper/NoteActivity.java
+++ app/src/main/java/com/example/androidassociatedeveloper/NoteActivity.java
@@ -1,16 +1,23 @@
 package com.example.androidassociatedeveloper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.List;
+
 public class NoteActivity extends AppCompatActivity {
-    private String[] courses = {"Course1", "Course2", "Course3"};
+    public static final String NOTE_INFO = "NOTE_INFO.com.example.androidassociatedeveloper";
+    private NoteInfo noteInfo;
+    private EditText note_text, note_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +26,34 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        note_text = findViewById(R.id.text_note_text);
+        note_title = findViewById(R.id.text_note_title);
+
+        List<CourseInfo> courseInfoList = DataManager.getInstance().getCourses();
+        Log.i("hdhdhd", String.valueOf(courseInfoList));
+
 //        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+//        ArrayAdapter<CourseInfo> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, courseInfoList);
 //        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        spinnerCourses.setAdapter(arrayAdapter);
+//
+//        readNoteContent();
+//
+//        displayNote(spinnerCourses, note_title, note_text);
+    }
+
+    private void readNoteContent(){
+        Intent intent = getIntent();
+        noteInfo = intent.getParcelableExtra(NOTE_INFO);
+    }
+
+    private void displayNote(Spinner spinner, EditText title, EditText text){
+       List<CourseInfo> courseInfoList = DataManager.getInstance().getCourses();
+       int courseIndex = courseInfoList.indexOf(noteInfo.getCourse());
+       spinner.setSelection(courseIndex);
+
+        title.setText(noteInfo.getTitle());
+        text.setText(noteInfo.getText());
     }
 
     @Override
--- app/src/main/java/com/example/androidassociatedeveloper/SecondFragment.java
+++ app/src/main/java/com/example/androidassociatedeveloper/SecondFragment.java
@@ -1,11 +1,13 @@
 package com.example.androidassociatedeveloper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -15,10 +17,13 @@ import androidx.navigation.fragment.NavHostFragment;
 import java.util.List;
 
 public class SecondFragment extends Fragment {
+    public static final String NOTE_INFO = "NOTE_INFO.com.example.androidassociatedeveloper";
+    private NoteInfo noteInfo;
+    private EditText note_text, note_title;
+    boolean isNewNote;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_second, container, false);
         return v;
     }
@@ -28,18 +33,31 @@ public class SecondFragment extends Fragment {
 
         List<CourseInfo> courseInfoList = DataManager.getInstance().getCourses();
         Log.i("hdhdhd", String.valueOf(courseInfoList));
-
+        note_text = view.findViewById(R.id.text_note_text);
+        note_title = view.findViewById(R.id.text_note_title);
         Spinner spinnerCourses = view.findViewById(R.id.spinner_courses);
         ArrayAdapter<CourseInfo> arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, courseInfoList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCourses.setAdapter(arrayAdapter);
 
-//        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(SecondFragment.this)
-//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-//            }
-//        });
+        readNoteContent();
+
+        if (!isNewNote)
+        displayNote(spinnerCourses, note_title, note_text);
+    }
+
+    private void readNoteContent(){
+        Intent intent = getActivity().getIntent();
+        noteInfo = intent.getParcelableExtra(NOTE_INFO);
+        isNewNote = noteInfo == null;
+    }
+
+    private void displayNote(Spinner spinner, EditText title, EditText text){
+        List<CourseInfo> courseInfoList = DataManager.getInstance().getCourses();
+        int courseIndex = courseInfoList.indexOf(noteInfo.getCourse());
+        spinner.setSelection(courseIndex);
+
+        title.setText(noteInfo.getTitle());
+        text.setText(noteInfo.getText());
     }
 }
